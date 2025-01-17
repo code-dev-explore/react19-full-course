aeb05c52b837a742ee865f12025b0ef2b468a2a2
code-dev-explore <code.dev.explore@gmail.com>
2025-01-17 12:38:18 +1100

    update 12

---
+++ 12-components/final/src/main.jsx
@@ -0,0 +1,67 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// const todo = (
+//   <div className="container">
+//     <div className="item">
+//       <div className="date">
+//         <div className="day">12</div>
+//         <div className="month">Jan</div>
+//       </div>
+//       <div className="content">
+//         <div className="title">
+//           <h2>Learn React</h2>
+//         </div>
+//         <div className="desc">Study Lesson 11 on CodeX channel.</div>
+//       </div>
+//     </div>
+//     <div className="item">
+//       <div className="date">
+//         <div className="day">13</div>
+//         <div className="month">Jan</div>
+//       </div>
+//       <div className="content">
+//         <div className="title">
+//           <h2>Learn HTML</h2>
+//         </div>
+//         <div className="desc">Study Lesson 25 on CodeX channel.</div>
+//       </div>
+//     </div>
+//     <div className="item">
+//       <div className="date">
+//         <div className="day">18</div>
+//         <div className="month">Jan</div>
+//       </div>
+//       <div className="content">
+//         <div className="title">
+//           <h2>Watch NFL</h2>
+//         </div>
+//         <div className="desc">Study Lesson 11 on CodeX channel.</div>
+//       </div>
+//     </div>
+//     <div className="item">
+//       <div className="date">
+//         <div className="day">22</div>
+//         <div className="month">Jan</div>
+//       </div>
+//       <div className="content">
+//         <div className="title">
+//           <h2>Read a book</h2>
+//         </div>
+//         <div className="desc">Harry Potter</div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+// function App() {
+//   return <h1>Hello React!!</h1>;
+// }
+
+createRoot(document.getElementById("root")).render(<App />);
+
+Func A() {
+  .........
+  .........
+}
